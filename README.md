27b379e7d095ef5a400f195c0f2cddd614781416
ccummings <lurkortv@gmail.com>
2019-08-07 17:22:22 -0700

    Completed 7 questions quiz, added lists and updated some CSS for those

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# 2about-me
+# about-me2
+This is the about me website for Christopher Cummings.
+
+v2 additions: 
+    Javascript 5 questions with a prompt.
+    Some CSS styling.
+
+v3 additions:
+    Created new GITHUB to create a master branch.
+    Adding lists for education and career.
+    Adding two more questions to guessing game, 5 yes or no and two answer.
+    Updated CSS
+    Added ol and ul for favorite food, education and work experience
--- js/app.js
+++ js/app.js
@@ -5,68 +5,108 @@ var gameConfirm = confirm('Hello ' + username + ', would you like to play a gues
 console.log('The user name: ' + username);
 
 if (gameConfirm){
-  alert('You will have three tries for each question before it moves onto the next!');
-  for (var i = 1; i <= 3; i++){
-    var q1 = prompt('How old was I when I first used a computer mouse?');
-    if (parseInt(q1) === 2){
+  var correctAnswers = 0; //number of correct answers
+
+  var q1 = prompt('Is it true that I\'ve lived in Hawaii? (y/n)');
+  if ((q1.toLowerCase()).charAt(0) === 'y'){
+    alert('Correct! I lived there for a couple of years. Mahalo for taking this quiz!');
+    console.log('Question 1 Correct Answer: ' + q1);
+    correctAnswers++;
+  }
+  else {
+    alert('Incorrect! I lived in Hawaii for a couple of years!');
+    console.log('Question 1 Incorrect Answer: ' + q1);
+  }
+
+  var q2 = prompt('Is my favorite sport football? (y/n)');
+  if ((q2.toLowerCase()).charAt(0) === 'n'){
+    alert('Correct! My favorite sport is actually tennis!');
+    correctAnswers++;
+    console.log('Question 2 Correct Answer: ' + q2);
+  }
+  else {
+    alert('Incorrect! My favorite sport is actually tennis!');
+    console.log('Question 2 Incorrect Answer: ' + q2);
+  }
+
+  var q3 = prompt('Do I love beer?');
+  if ((q3.toLowerCase()).charAt(0) === 'y'){
+    alert('Correct! I can\'t help it if it tastes so good.');
+    console.log('Question 3 Correct Answer: ' + q3);
+    correctAnswers++;
+  }
+  else {
+    alert('Incorrect! How could I hate something so delicious?');
+    console.log('Question 3 Incorrect Answer: ' + q3);
+  }
+
+  var q4 = prompt('Have I ever been stung by a bee? (y/n)');
+  if ((q4.toLowerCase()).charAt(0) === 'n'){
+    alert('Correct! Knock on wood.');
+    console.log('Question 4 Correct Answer: ' + q4);
+    correctAnswers++;
+  }
+  else {
+    alert('Incorrect! Bees love me! (Please don\'t sting me)');
+    console.log('Question 4 Incorrect Answer: ' + q4);
+  }
+
+  var q5 = prompt('Have I ever played hockey?');
+  if ((q5.toLowerCase()).charAt(0) === 'n'){
+    alert('Correct! I have never played but hockey games are fun!');
+    console.log('Question 5 Correct Answer: ' + q5);
+    correctAnswers++;
+  }
+  else {
+    alert('Incorrect! I have never played but hockey games are fun to watch!');
+    console.log('Question 5 Incorrect Answer: ' + q5);
+  }
+
+  // start of type in answers
+  for (var i = 1; i <= 4; i++){
+    var q6 = prompt('How old was I when I first used a computer mouse? (You have 4 tries!)');
+    if (parseInt(q6) === 2){
       alert('Correct! I\'ve been on the computer for way too long!');
-      console.log('Question 1 Correct Answer: ' + q1);
+      console.log('Question 6 Correct Answer: ' + q6);
+      correctAnswers++;
       break;
     }
     else {
-      alert('Try again! This is try number ' + i + '.');
-      console.log('Question 1 Incorrect Answer Attempt ' + i + ': ' + q1);
+      if (q6 < 2){
+        alert('Try again! This is try number ' + i + '. Try guessing higher!');
+        console.log('Question 6 Incorrect Answer Attempt ' + i + ': ' + q6);
+      }
+      else {
+        alert('Try again! This is try number ' + i + '. Try guessing lower!');
+        console.log('Question 6 Incorrect Answer Attempt ' + i + ': ' + q6);
+      }
     }
   }
-  for (var j = 1; j <= 3; j++){
-    var q2 = prompt('Is it true that I\'ve lived in Hawaii? (y/n)');
-    if ((q2.toLowerCase()).charAt(0) === 'y'){
-      alert('Correct! I lived there for a couple of years. Mahalo for taking this quiz!');
-      console.log('Question 1 Correct Answer: ' + q2);
-      break;
+
+  for (var j = 1; j <= 6; j++){
+    var q7 = prompt('What cities have I lived in? (You have 6 tries!)');
+
+    var livedCities = ['seattle', 'honolulu', 'mill creek', 'spokane'];
+
+    for (var k = 0; k <= 3; k++){
+      if(livedCities[k] === q7.toLowerCase()){
+        livedCities.splice(k, 1);
+        alert('Correct! I have also lived in ' + livedCities);
+        j = 7;
+        correctAnswers++;
+        break;
+      }
+    }
+
+
+    if (j === 6){
+      alert('Nice try! The correct answers were: Honolulu, Mill Creek, Spokane and Seattle!');
     }
     else {
-      alert('Try again! This is try number ' + j + '.');
-      console.log('Question 1 Incorrect Answer Attempt ' + j + ': ' + q2);
+      alert('Incorrect! Try again!');
     }
   }
-  for (var k = 1; k <= 3; k++){
-    var q3 = prompt('Is my favorite sport football? (y/n)');
-    if ((q3.toLowerCase()).charAt(0) === 'n'){
-      alert('Correct! My favorite sport is actually tennis!');
-      console.log('Question 1 Correct Answer: ' + q3);
-      break;
-    }
-    else {
-      alert('Try again! This is try number ' + k + '.');
-      console.log('Question 1 Incorrect Answer Attempt ' + k + ': ' + q3);
-    }
-  }
-  for (var l = 1; l <= 3; l++){
-    var q4 = prompt('Do I love beer?');
-    if ((q4.toLowerCase()).charAt(0) === 'y'){
-      alert('Correct! I can\'t help it if it tastes so good.');
-      console.log('Question 1 Correct Answer: ' + q4);
-      break;
-    }
-    else {
-      alert('Try again! This is try number ' + l + '.');
-      console.log('Question 1 Incorrect Answer Attempt ' + l + ': ' + q4);
-    }
-  }
-  for (var m = 1; m <= 3; m++){
-    var q5 = prompt('Have I ever been stung by a bee? (y/n)');
-    if ((q5.toLowerCase()).charAt(0) === 'n'){
-      alert('Correct! Knock on wood.');
-      alert('Thanks for taking my quiz, ' + username + '! Enjoy my page!');
-      console.log('Question 1 Correct Answer: ' + q5);
-      break;
-    }
-    else {
-      alert('Try again! This is try number ' + m + '.');
-      console.log('Question 1 Incorrect Answer Attempt ' + m + ': ' + q5);
-    }
-  }
+  alert('Thanks for taking my quiz, ' + username + '! Your score was ' + correctAnswers + ' out of 7! Enjoy my page!');
 }
 else {
   alert('Sounds good! Welcome to my page ' + username + ' !');
